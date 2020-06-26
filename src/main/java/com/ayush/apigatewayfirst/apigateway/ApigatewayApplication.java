37ec772e828e37469541236ee581aea234c32d57
AyushAkarsh <ayush.akarsh@iitkgp.ac.in>
2020-06-27 00:32:42 +0530

    Routing for 4 services done

---
+++ src/main/java/com/ayush/apigatewayfirst/apigateway/ApigatewayApplication.java
@@ -0,0 +1,22 @@
+package com.ayush.apigatewayfirst.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+// @EnableDiscoveryClient
+@SpringBootApplication
+public class ApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayApplication.class, args);
+    }
+    @Bean
+  public ZuulLoggingFilter simpleFilter() {
+    return new ZuulLoggingFilter();
+  }
+
+}
